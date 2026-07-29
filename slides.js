d63a578d3de79263af4b926d731746dbdf4ed1e3
Olutobi Olusesi <olusesiolutobi@gmail.com>
2026-07-29 22:40:02 +0100

    Update slides.js

--- slides.js
+++ slides.js
@@ -7,10 +7,10 @@ let dont // stop doing the sliding
 let mainObject = document.getElementById("you");
 // class values stored in an array
 let myslides = [
-    "A2.png", "A3.png", "A4.png", "A5.png", "A6.png", 
-    "A7.png", "A8.png", "A10.png", "A11.png", "A12.png", 
-    "A13.png", "A14.png", "A15.png", "A18.png", 
-    "A19.png", "A20.png"
+    "A1.png", "A2.png", "A3.png", "A4.png", "A5.png", "A6.png", 
+    "A7.png", "A8.png", "A9.png", "A10.png", "A11.png", "A12.png", 
+    "A13.png", "A14.png", "A15.png", "A16.png", "A17.png", "A18.png", 
+    "A19.png", "A20.png", "A21.png", "A22.png"
 ]
 // this function gives random value to select any key
 numSl = 0;
@@ -22,14 +22,28 @@ function numberingSlides() {
     }
     return numSlides;  
 }
-
+function alignment() {
+    let vertical = [
+        "A1.png", "A9.png", "A16.png", "A17.png", "A21.png", "A22.png"
+    ];
+    if (vertical.includes(answer)) {
+       return "vertical"
+    }
+    else {
+        return "horizontal"
+    }
+}
 //this create the slides 
 function slideIt() {
     numberingSlides();
     answer = myslides[numSlides];
     numSl++;
     mainObject.src = answer.toString();
+    let changeAlignment = alignment();
+    mainObject.className = changeAlignment;
 }
+
+
 
 //this function set the background picture to slide one another
 function action() {
